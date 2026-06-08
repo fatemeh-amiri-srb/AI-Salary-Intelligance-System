b7a96ba7ad6b07d4420e063ccf189c9f46397590
Fatemeh Amiri <fatemehamiri.srb@gmail.com>
2026-06-08 08:56:53 +0330

    feat: standardize salary to yearly scale and handle hourly wages

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from src.data_loader import load_data
 from src.eda import basic_info
+from src.cleaner import clean_salary
 
 df=load_data("data/raw/glassdoor_jobs.csv")
 basic_info(df)
+# print(df["Salary Estimate"].head(20))
+# print(df["Salary Estimate"].sample(20).tolist())
+
+df=clean_salary(df)
+df[["Salary Estimate", "hourly", "avg_salary"]].head(10)
+print(df["hourly"].value_counts())
+print(df["avg_salary"].describe())
+# print(df[["Salary Estimate",
+#           "min_salary",
+#           "max_salary",
+#           "salary"]].head(20))
--- src/cleaner.py
+++ src/cleaner.py
@@ -1,18 +1,40 @@
 import pandas as pd
+import numpy as np
+import re
 
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Clean raw Glassdoor data
-    """
 
-    df = df.dropna()
+def clean_salary(df: pd.DataFrame) -> pd.DataFrame:
 
-    # remove extreme salaries
-    df = df[df["salary"] < df["salary"].quantile(0.99)]
+    df = df[df["Salary Estimate"] != "-1"].copy()
 
-    # normalize text
-    df["job_title"] = df["job_title"].str.lower()
-    df["company"] = df["company"].str.lower()
-    df["location"] = df["location"].str.lower()
+    # flag hourly salaries
+    df["hourly"] = df["Salary Estimate"].str.contains("Per Hour", na=False)
+
+    # remove text noise
+    salary = (
+        df["Salary Estimate"]
+        .str.replace(r"\(.*?\)", "", regex=True)
+        .str.replace("Employer Provided Salary:", "", regex=False)
+        .str.replace("Per Hour", "", regex=False)
+        .str.replace("$", "", regex=False)
+        .str.replace("K", "", regex=False)
+        .str.strip()
+    )
+
+    # split range
+    salary_split = salary.str.split("-", expand=True)
+
+    df["min_salary"] = salary_split[0].astype(float)
+    df["max_salary"] = salary_split[1].astype(float)
+
+    # average salary
+    df["avg_salary"] = (df["min_salary"] + df["max_salary"]) / 2
+
+    # convert hourly → yearly
+    df["avg_salary"] = np.where(
+        df["hourly"] == True,
+        df["avg_salary"] * 40 * 52,
+        df["avg_salary"] * 1000  # چون K داریم
+    )
 
     return df
